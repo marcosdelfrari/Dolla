371fdaa81dfe0ca6821f40f7df8a8e416c9cd847
Marcos Lucas <afk.marcos@gmail.com>
2023-09-04 06:28:47 -0300

    keyframe alternative

--- src/app/components/premium/artistas-premium/artistas-premium.component.ts
+++ src/app/components/premium/artistas-premium/artistas-premium.component.ts
@@ -3,13 +3,13 @@ import { Component, ElementRef, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-artistas-premium',
   templateUrl: './artistas-premium.component.html',
-  styleUrls: ['./artistas-premium.component.scss']
+  styleUrls: ['./artistas-premium.component.scss'],
 })
 export class ArtistasPremiumComponent {
   photos = [
-    { src: 'assets/imgs/larissa.jpg', caption: 'Legenda da Larissa', showCaption: false },
-    { src: 'assets/imgs/flavia.jpeg', caption: 'Legenda da Flavia', showCaption: false },
-    { src: 'assets/imgs/marcos.png', caption: 'Legenda do Marcos', showCaption: false }
+    { src: 'assets/imgs/larissa.jpg' },
+    { src: 'assets/imgs/flavia.jpeg' },
+    { src: 'assets/imgs/marcos.png' },
   ];
 
   toggleCaption(photo: any) {
